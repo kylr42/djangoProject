9a497cc97bb164df9e45e4b3600163221e17c4e6
Jimbo Siona <jsiona@et-n4.kzn.21-school.ru>
2021-07-17 07:15:47 +0300

    added form for add news

--- news/urls.py
+++ news/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', index, name='home'),
     path('category/<int:category_id>/', get_category, name='category'),
     path('news/<int:news_id>/', get_news, name='news'),
+    path('news/add_news/', add_news, name='add_news'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import News, Category
-
+from .forms import NewsForm
 
 def index(request):
 	news = News.objects.all()
@@ -25,3 +25,11 @@ def get_category(request, category_id):
 def get_news(request, news_id):
 	news_item = get_object_or_404(News, pk=news_id)
 	return render(request, 'news/get_news.html', {'news_item': news_item})
+
+
+def add_news(request):
+	if request.method == 'POST':
+		pass
+	else:
+		form = NewsForm
+	return render(request, 'news/add_news.html', {'form': form})
